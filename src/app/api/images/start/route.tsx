aaa53c375d7aae21a079dd190da67fea1a9f75cc
port <108868128+portdeveloper@users.noreply.github.com>
2024-02-12 18:12:14 +0300

    comment out nft fetching logic

--- src/app/api/images/start/route.tsx
+++ src/app/api/images/start/route.tsx
@@ -53,40 +53,40 @@ const getTokens = async (addy: Address) => {
   }
 };
 
-const getNfts = async (addy: Address) => {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      "x-api-key": process.env["OPENSEA_API_KEY"] || "default-key",
-    },
-  };
+// const getNfts = async (addy: Address) => {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       accept: "application/json",
+//       "x-api-key": process.env["OPENSEA_API_KEY"] || "default-key",
+//     },
+//   };
 
-  try {
-    const response = await fetch(
-      `https://api.opensea.io/api/v2/chain/ethereum/account/${addy}/nfts`,
-      options
-    );
-    const data = await response.json();
+//   try {
+//     const response = await fetch(
+//       `https://api.opensea.io/api/v2/chain/ethereum/account/${addy}/nfts`,
+//       options
+//     );
+//     const data = await response.json();
 
-    if (data.nfts && data.nfts.length > 0) {
-      const nftData = [];
-      for (let i = 0; i < Math.min(1, data.nfts.length); i++) {
-        const nft = data.nfts[i];
-        if (nft.image_url && nft.identifier !== "0") {
-          nftData.push({
-            imageUrl: nft.image_url,
-            contract: nft.contract,
-            identifier: nft.identifier,
-          });
-        }
-      }
-      return nftData;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
+//     if (data.nfts && data.nfts.length > 0) {
+//       const nftData = [];
+//       for (let i = 0; i < Math.min(1, data.nfts.length); i++) {
+//         const nft = data.nfts[i];
+//         if (nft.image_url && nft.identifier !== "0") {
+//           nftData.push({
+//             imageUrl: nft.image_url,
+//             contract: nft.contract,
+//             identifier: nft.identifier,
+//           });
+//         }
+//       }
+//       return nftData;
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
@@ -112,7 +112,7 @@ export async function GET(req: NextRequest) {
     ? `https://metadata.ens.domains/mainnet/avatar/${addyOrEns}`
     : blo(addyOrEns as Address);
   const tokens = await getTokens(addy as Address); // Fetch token balances
-  const nfts = await getNfts(addy as Address);
+  // const nfts = await getNfts(addy as Address);
 
   const filteredTokens = tokens
     ? tokens
@@ -174,7 +174,7 @@ export async function GET(req: NextRequest) {
               </div>
 
               <div tw="bg-white text-4xl m-8 mt-0 p-12 h-[350px] rounded-16 shadow-2xl flex items-center justify-center">
-                <div
+                {/* <div
                   style={{
                     gap: "16px",
                   }}
@@ -188,7 +188,7 @@ export async function GET(req: NextRequest) {
                       height={200}
                     />
                   ))}
-                </div>
+                </div> */}
               </div>
             </div>
             <div tw="bg-white text-4xl m-8 ml-0 h-[590px] w-[480px] overflow-hidden p-8 rounded-16 shadow-2xl flex flex-col ">
