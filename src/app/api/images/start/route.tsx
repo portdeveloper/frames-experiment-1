ac2c4b98e8f6f437736dd53d5ef05c1455f3ac72
port <108868128+portdeveloper@users.noreply.github.com>
2024-02-12 19:18:36 +0300

    Show everything except the qr

--- src/app/api/images/start/route.tsx
+++ src/app/api/images/start/route.tsx
@@ -19,7 +19,7 @@ const publicClient = createPublicClient({
   transport: http(),
 });
 
-// const client = new CovalentClient(process.env["COVALENT_API_KEY"] as string);
+const client = new CovalentClient(process.env["COVALENT_API_KEY"] as string);
 
 const interRegPath = join(process.cwd(), "public/Inter-Regular.ttf");
 let interReg = fs.readFileSync(interRegPath);
@@ -36,57 +36,57 @@ let interBold = fs.readFileSync(interBoldPath);
 //   }
 // }
 
-// const getTokens = async (addy: Address) => {
-//   const res = await client.BalanceService.getTokenBalancesForWalletAddress(
-//     "eth-mainnet",
-//     addy,
-//     {
-//       nft: false,
-//       noSpam: true,
-//     }
-//   );
-//   if (res.data && res.data.items) {
-//     const filteredTokens = res.data.items
-//       ? res.data.items.filter((token) => token.quote !== 0)
-//       : [];
-//     return filteredTokens;
-//   }
-// };
+const getTokens = async (addy: Address) => {
+  const res = await client.BalanceService.getTokenBalancesForWalletAddress(
+    "eth-mainnet",
+    addy,
+    {
+      nft: false,
+      noSpam: true,
+    }
+  );
+  if (res.data && res.data.items) {
+    const filteredTokens = res.data.items
+      ? res.data.items.filter((token) => token.quote !== 0)
+      : [];
+    return filteredTokens;
+  }
+};
 
-// const getNfts = async (addy: Address) => {
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       accept: "application/json",
-//       "x-api-key": process.env["OPENSEA_API_KEY"] || "default-key",
-//     },
-//   };
+const getNfts = async (addy: Address) => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "x-api-key": process.env["OPENSEA_API_KEY"] || "default-key",
+    },
+  };
 
-//   try {
-//     const response = await fetch(
-//       `https://api.opensea.io/api/v2/chain/ethereum/account/${addy}/nfts`,
-//       options
-//     );
-//     const data = await response.json();
+  try {
+    const response = await fetch(
+      `https://api.opensea.io/api/v2/chain/ethereum/account/${addy}/nfts`,
+      options
+    );
+    const data = await response.json();
 
-//     if (data.nfts && data.nfts.length > 0) {
-//       const nftData = [];
-//       for (let i = 0; i < Math.min(1, data.nfts.length); i++) {
-//         const nft = data.nfts[i];
-//         if (nft.image_url && nft.identifier !== "0") {
-//           nftData.push({
-//             imageUrl: nft.image_url,
-//             contract: nft.contract,
-//             identifier: nft.identifier,
-//           });
-//         }
-//       }
-//       return nftData;
-//     }
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
+    if (data.nfts && data.nfts.length > 0) {
+      const nftData = [];
+      for (let i = 0; i < Math.min(1, data.nfts.length); i++) {
+        const nft = data.nfts[i];
+        if (nft.image_url && nft.identifier !== "0") {
+          nftData.push({
+            imageUrl: nft.image_url,
+            contract: nft.contract,
+            identifier: nft.identifier,
+          });
+        }
+      }
+      return nftData;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
@@ -104,39 +104,39 @@ export async function GET(req: NextRequest) {
   }
 
   // let qrCodeBase64 = await generateQRCodeBase64(addy as Address);
-  // const balance = await publicClient.getBalance({
-  //   address: addy as Address,
-  // });
-  // const isEns = addyOrEns.endsWith(".eth");
-  // const imageSrc = isEns
-  //   ? `https://metadata.ens.domains/mainnet/avatar/${addyOrEns}`
-  //   : blo(addyOrEns as Address);
-  // const tokens = await getTokens(addy as Address); // Fetch token balances
-  // const nfts = await getNfts(addy as Address);
+  const balance = await publicClient.getBalance({
+    address: addy as Address,
+  });
+  const isEns = addyOrEns.endsWith(".eth");
+  const imageSrc = isEns
+    ? `https://metadata.ens.domains/mainnet/avatar/${addyOrEns}`
+    : blo(addyOrEns as Address);
+  const tokens = await getTokens(addy as Address); // Fetch token balances
+  const nfts = await getNfts(addy as Address);
 
-  // const filteredTokens = tokens
-  //   ? tokens
-  //       .slice(0, 10)
-  //       .filter((t) => t.quote != null && t.quote.toFixed(0) !== "0")
-  //   : [];
+  const filteredTokens = tokens
+    ? tokens
+        .slice(0, 10)
+        .filter((t) => t.quote != null && t.quote.toFixed(0) !== "0")
+    : [];
 
-  // const tokensInfo = filteredTokens.map((token) => ({
-  //   name: token.contract_ticker_symbol,
-  //   balance: formatTokenBalance(
-  //     token.balance as bigint,
-  //     token.contract_decimals
-  //   ),
-  //   quote: `$${Number(token.quote_rate).toFixed(2)}`, // Assuming quote_rate is available and represents the USD value
-  // }));
+  const tokensInfo = filteredTokens.map((token) => ({
+    name: token.contract_ticker_symbol,
+    balance: formatTokenBalance(
+      token.balance as bigint,
+      token.contract_decimals
+    ),
+    quote: `$${Number(token.quote_rate).toFixed(2)}`, // Assuming quote_rate is available and represents the USD value
+  }));
 
-  // console.log("tokensInfo", tokensInfo);
+  console.log("tokensInfo", tokensInfo);
 
-  // const truncateBalance = (balance: string) => {
-  //   const balanceStr = balance.toString();
-  //   return balanceStr.length > 8
-  //     ? `${balanceStr.substring(0, 8)}...`
-  //     : balanceStr;
-  // };
+  const truncateBalance = (balance: string) => {
+    const balanceStr = balance.toString();
+    return balanceStr.length > 8
+      ? `${balanceStr.substring(0, 8)}...`
+      : balanceStr;
+  };
 
   return new ImageResponse(
     (
@@ -155,18 +155,18 @@ export async function GET(req: NextRequest) {
             <div tw="flex flex-col">
               <div tw="flex">
                 <div tw="bg-white text-4xl m-8 p-8 h-[222px] rounded-16 shadow-2xl flex items-center justify-between ">
-                  {/* <img
+                  <img
                     src={imageSrc}
                     width={150}
                     height={150}
                     tw="rounded-full"
-                  /> */}
-                  {/* <div tw="flex flex-col ml-8">
+                  />
+                  <div tw="flex flex-col ml-8">
                     <strong>{addyOrEns}</strong>
                     <span tw="mt-2">
                       Balance: {Number(formatEther(balance)).toFixed(4)} ETH
                     </span>
-                  </div> */}
+                  </div>
                 </div>
                 <div tw="bg-white text-4xl m-8 ml-0 p-8 h-[222px] rounded-16 shadow-2xl flex items-center justify-between ">
                   {/* <img src={qrCodeBase64} width={190} height={190} /> */}
@@ -174,7 +174,7 @@ export async function GET(req: NextRequest) {
               </div>
 
               <div tw="bg-white text-4xl m-8 mt-0 p-12 h-[350px] rounded-16 shadow-2xl flex items-center justify-center">
-                {/* <div
+                <div
                   style={{
                     gap: "16px",
                   }}
@@ -188,13 +188,13 @@ export async function GET(req: NextRequest) {
                       height={200}
                     />
                   ))}
-                </div> */}
+                </div>
               </div>
             </div>
             <div tw="bg-white text-4xl m-8 ml-0 h-[590px] w-[480px] overflow-hidden p-8 rounded-16 shadow-2xl flex flex-col ">
               <div tw="flex flex-col w-full">
                 {/* Header */}
-                {/* <div
+                <div
                   style={{
                     display: "flex",
                     width: "100%",
@@ -208,10 +208,10 @@ export async function GET(req: NextRequest) {
                   <span style={{ flex: 1, fontSize: "18px" }}>
                     Balance in USD
                   </span>
-                </div> */}
+                </div>
 
                 {/* Rows */}
-                {/* {tokensInfo.map((token, index) => (
+                {tokensInfo.map((token, index) => (
                   <div
                     key={index}
                     style={{
@@ -229,7 +229,7 @@ export async function GET(req: NextRequest) {
                     </span>
                     <span style={{ flex: 1 }}>≈{token.quote}</span>
                   </div>
-                ))} */}
+                ))}
               </div>
             </div>
           </div>
@@ -256,10 +256,10 @@ export async function GET(req: NextRequest) {
     }
   );
 }
-// // This is a helper function similar to formatTokenBalance for API route context
-// const formatTokenBalance = (balance: bigint, decimals: number) => {
-//   const divisor = BigInt(Math.pow(10, decimals));
-//   const integerPart = balance / divisor;
-//   const fractionalPart = balance % divisor;
-//   return `${integerPart}.${fractionalPart.toString().padStart(decimals, "0").slice(0, 2)}`;
-// };
+// This is a helper function similar to formatTokenBalance for API route context
+const formatTokenBalance = (balance: bigint, decimals: number) => {
+  const divisor = BigInt(Math.pow(10, decimals));
+  const integerPart = balance / divisor;
+  const fractionalPart = balance % divisor;
+  return `${integerPart}.${fractionalPart.toString().padStart(decimals, "0").slice(0, 2)}`;
+};
