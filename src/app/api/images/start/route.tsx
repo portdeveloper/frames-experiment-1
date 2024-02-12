1104e4d1dd0190ea7e7299f7cb20817a7e763072
port <108868128+portdeveloper@users.noreply.github.com>
2024-02-12 23:52:19 +0300

    Increase timeout

--- src/app/api/images/start/route.tsx
+++ src/app/api/images/start/route.tsx
@@ -18,7 +18,7 @@ const publicClient = createPublicClient({
   transport: http(),
 });
 
-export const maxDuration = 20;
+export const maxDuration = 60;
 
 const client = new CovalentClient(process.env["COVALENT_API_KEY"] as string);
 
